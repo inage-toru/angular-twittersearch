65950bdd213b568445c0e274c3b39805b3db2da2
Toru Inage <inage.toru@classmethod.jp>
2012-06-26 19:10:30 +0900

    first commit

---
+++ app/js/search.js
@@ -0,0 +1,10 @@
+﻿function TwitterSearchCtrl($scope, $http, $log) {
+  $scope.doSearch = function() {
+    var uri ='http://search.twitter.com/search.json?q='
+      + encodeURIComponent($scope.query)
+      + '&callback=JSON_CALLBACK';
+    $http.jsonp(uri).success(function(data) {
+      $scope.results = data.results;
+    });
+  };
+}
